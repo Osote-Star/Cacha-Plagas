56a600906fd191c9e0b8fb6a23c166fbfd1d821b
TheOnlyRadiance <rodrigomorquecho3@gmail.com>
2025-02-10 11:14:10 -0700

    quitar boton creacion de cuenta y agregacion de labels

--- obj/Debug/net8.0-android/CachaPlagas.AssemblyInfo.cs
+++ obj/Debug/net8.0-android/CachaPlagas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CachaPlagas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+8a028ea9e1960fa5378a865af805effd4da67ff1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+4249ba3a114fe8e40672caaffe4a84f1fea7e56a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CachaPlagas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CachaPlagas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
