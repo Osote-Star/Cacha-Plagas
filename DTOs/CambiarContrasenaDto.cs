a81337f6805f21c1cb905e04eeb24b6aa14740cc
Osote-Star <5321100090@correcaminos.uts.edu.mx>
2025-04-07 13:09:43 -0700

    Agregado del .gitignore

---
+++ DTOs/CambiarContrasenaDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CachaPlagas.DTOs
+{
+    public class CambiarContrasenaDto
+    {
+        public string Email { get; set; }
+        public string Contrasena { get; set; }
+    }
+}
